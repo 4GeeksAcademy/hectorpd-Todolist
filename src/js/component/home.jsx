447f5d34b26ddda7265e481354f12f8c0858a81c
Hector Padron <helpad93@gmail.com>
2023-07-13 19:55:02 +0000

    iniciado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [list, setList] = useState([]);
+	const handleKeyDown = (e) => {
+		if(e.key === "Enter") {
+			setList(list.concat(inputValue));
+		}
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>Lista</h1>
+			<ul>
+				<li><input type="text" onChange={(e) => setInputValue(e.target.value)} onKeyDown={(e) => handleKeyDown(e)} value={inputValue}  placeholder="Añadir tarea" /></li>
+				{list.map((item, index) => (
+					<li> {item} {" "} <i class="fa-solid fa-x" 
+							onClick={() => setList(list.filter((l, currentIndex) => index != currentIndex))}></i>
+					
+					</li>
+				))}
+				
+			</ul>
+			<div> {list.length} Task </div>
 		</div>
 	);
 };
