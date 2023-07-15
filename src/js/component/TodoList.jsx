df44e0f7cd23df9ae3dc820eb8709783d38f3df0
Hector Padron <helpad93@gmail.com>
2023-07-15 19:56:54 +0000

    change: final modification in TodoList and index

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -3,44 +3,58 @@ import React, { useState } from "react";
 
 
 
+
 export const TodoList = () => {
     const [inputValue, setInputValue] = useState("");
 	const [list, setList] = useState([]);
+    const [isEmpty, setIsEmpty] = useState(true);
 	const handleKeyDown = (e) => {
 		if(e.key === "Enter") {
             e.preventDefault();
             if (inputValue.trim() !== ""){
             setList(list.concat(inputValue));
             setInputValue("");
+            setIsEmpty(false);
             }    
 		}
 	}
     const handleDelete = (index) => {
         setList
     setList(list.filter((_, currentIndex) => index !== currentIndex));
+    setIsEmpty(list.length === 1);
     };
-    const footer = () => {
-        {list === 0 ? (Add task)};
-    }
+    
     return (
-    <div className="container">
-        <h1>To do List</h1>
-        <ul>
-            <h3>New List</h3>
-            <li>
-                <input className="input" type="text" 
-                onChange={(e) => setInputValue(e.target.value)} 
-                onKeyDown={(e) => handleKeyDown(e)} value={inputValue}  
-                placeholder="Añadir tarea" />
-            </li>
-            
-            {list.map((item, index) => (
-                <li className="newtask"> {item} {" "} <i className="fa-solid fa-times" onClick={() => handleDelete(index)}></i>
-                </li>
-            ))}
-            <div className="listfooter"> {list.length} Task </div>
-        </ul>
         
+    <div className="main">
+        <h1>To do List</h1>
+        <div className="container">
+            <h3>New List</h3>
+            <ul>
+                <li>
+                    <input
+                    className="input"
+                    type="text"
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyDown={(e) => handleKeyDown(e)}
+                    value={inputValue}
+                    placeholder="New Task"/>
+                </li>
+                {list.map((item, index) => (
+                    <li className="newtask">
+                        {" "}
+                        {item}{" "}
+                            <i className="fas fa-times icon"
+                            onClick={() => handleDelete(index)}>
+                            </i>
+                    </li>
+                ))}
+            </ul>
+            {isEmpty && <div className="listfooter">No tasks, Add task</div>}
+            {!isEmpty && <div className="listfooter"> {list.length} Task </div>}
+        </div>
+        <div className="pagetwo"></div>
+        <div className="pagethree"></div>
     </div>
-    )
+    );
 }
